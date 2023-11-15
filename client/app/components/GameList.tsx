60c171f0d3daeb8dd2e22ee3916c1436424a2271
rickardpeters <77685817+rickardpeters@users.noreply.github.com>
2023-11-15 20:11:27 +0100

    added loading animation for gamelist

--- client/app/components/GameList.tsx
+++ client/app/components/GameList.tsx
@@ -6,6 +6,10 @@ import { GameContext } from "../context/GameContext";
 const GameList = () => {
   const { games } = useContext(GameContext);
 
+  if (!games) {
+    return <div className="grid place-items-center h-[70vh] text-3xl font-agbalumo animate-pulse">Loading...</div>;
+  }
+
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grplace-items-center w-screen">
